9490459b702ea19da3f73ee1b29bc49e613e508b
kalavarda <kulerda.kalavarda@gmail.com>
2022-03-26 01:19:57 +0300

    init commit

---
+++ Kalantyr.Web/IRequestEnricher.cs
@@ -0,0 +1,9 @@
+﻿using System.Net.Http.Headers;
+
+namespace Kalantyr.Web
+{
+    public interface IRequestEnricher
+    {
+        void Enrich(HttpRequestHeaders requestHeaders);
+    }
+}
---
+++ Kalantyr.Web/WebClientBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Http;
+
+namespace Kalantyr.Web
+{
+    public abstract class HttpClientBase
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IRequestEnricher _requestEnricher;
+        private const string ContentType = "application/json";
+
+        protected HttpClientBase(IHttpClientFactory httpClientFactory, IRequestEnricher requestEnricher)
+        {
+            _httpClientFactory = httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory));
+            _requestEnricher = requestEnricher;
+        }
+    }
+}
