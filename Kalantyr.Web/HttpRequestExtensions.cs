1916d10e3dea88a02d77d8f5bcc5dde44dbe5be0
kalavarda <kulerda.kalavarda@gmail.com>
2022-06-02 11:13:12 +0300

    1

---
+++ Kalantyr.Web/HttpRequestExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Kalantyr.Web
+{
+    public static class HttpRequestExtensions
+    {
+        public static string GetAuthToken(this HttpRequest request)
+        {
+            if (request.Headers.TryGetValue("Authorization", out var token))
+                return token.ToString().Replace("Bearer ", string.Empty);
+            return null;
+        }
+
+        public static string GetAppKey(this HttpRequest request)
+        {
+            if (request.Headers.TryGetValue("App-Key", out var token))
+                return token;
+            return null;
+        }
+    }
+}
